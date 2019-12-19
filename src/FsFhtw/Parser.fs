78dd3473e3183906753c17c86689db8eb8d9c311
WalternativE <gregor.beyerle@gmail.com>
2019-12-19 07:40:40 +0100

    Parameterize parsing of int values to decrease code duplication

--- src/FsFhtw/Parser.fs
+++ src/FsFhtw/Parser.fs
@@ -9,15 +9,17 @@ let safeEquals (it : string) (theOther : string) =
 let HelpLabel = "Help"
 
 let (|Increment|Decrement|IncrementBy|DecrementBy|Help|ParseFailed|) (input : string) =
+    let tryParseInt (arg : string) valueConstructor =
+        let (worked, arg') = Int32.TryParse arg
+        if worked then valueConstructor arg' else ParseFailed
+
     let parts = input.Split(' ') |> List.ofArray
     match parts with
     | [ verb ] when safeEquals verb (nameof Domain.Increment) -> Increment
     | [ verb ] when safeEquals verb (nameof Domain.Decrement) -> Decrement
     | [ verb ] when safeEquals verb HelpLabel -> Help
     | [ verb; arg ] when safeEquals verb (nameof Domain.IncrementBy) ->
-        let (worked, arg') = Int32.TryParse arg
-        if worked then IncrementBy arg' else ParseFailed
+        tryParseInt arg (fun value -> IncrementBy value)
     | [ verb; arg ] when safeEquals verb (nameof Domain.DecrementBy) ->
-        let (worked, arg') = Int32.TryParse arg
-        if worked then DecrementBy arg' else ParseFailed
+        tryParseInt arg (fun value -> DecrementBy value)
     | _ -> ParseFailed
